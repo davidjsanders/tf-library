531f6c92e227b3c234b0c800b10f34fbe5dd2fc8
David Sanders <dsanderscanada@gmail.com>
2020-01-19 15:25:24 -0500

    Remove password from outputs

--- azure/jumpbox/outputs.tf
+++ azure/jumpbox/outputs.tf
@@ -2,10 +2,6 @@ output "private-ip" {
   value = module.jumpbox-nic.ips[0]
 }
 
-output "vm-password" {
-  value = random_password.password.result
-}
-
 output "public-ip" {
   value = module.jumpbox-pip.ip-addresses[0]
 }
