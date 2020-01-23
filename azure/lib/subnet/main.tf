41944a9c1771b876aa3160de8bb8400877cfd468
David Sanders <dsanderscanada@gmail.com>
2020-01-22 19:21:26 -0500

    Update lifecycle

--- azure/lib/subnet/main.tf
+++ azure/lib/subnet/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_subnet" "subnet" {
   address_prefix = var.subnet.address_prefix
   lifecycle {
-    ignore_changes = [route_table_id, network_security_group_id, ip_configurations]
+    ignore_changes = [route_table_id,network_security_group_id]
   }
 
   name = upper(
