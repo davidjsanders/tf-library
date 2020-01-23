950e73cc7249ad349360b7c769a81fd48ccff090
David Sanders <dsanderscanada@gmail.com>
2020-01-22 21:17:24 -0500

    Add server count to linux-server

--- azure/linux-jumpbox/server-linux.tf
+++ azure/linux-jumpbox/server-linux.tf
@@ -3,7 +3,6 @@ module "jumpbox-server" {
 
   linux-server = {
     admin-user              = var.jumpbox.admin-user
-    server-name             = var.jumpbox.jumpbox-name
     location                = var.jumpbox.location
     machine-size            = var.jumpbox.machine-size
     nsg-id                  = var.jumpbox.nsg-id
@@ -13,6 +12,8 @@ module "jumpbox-server" {
     public-key              = var.jumpbox.public-key
     randomizer              = var.jumpbox.randomizer
     rg-name                 = var.jumpbox.rg-name
+    server-count            = 1
+    server-name             = var.jumpbox.jumpbox-name
     storage-account-uri     = var.jumpbox.storage-account-uri
     storage-image-reference = var.jumpbox.storage-image-reference
     subnet-id               = var.jumpbox.subnet-id
--- azure/linux-server/variables.tf
+++ azure/linux-server/variables.tf
@@ -2,16 +2,14 @@ variable "linux-server" {
   type = object(
       {
           admin-user              = string
-          server-name             = string
           location                = string
           machine-size            = string
-          nsg-id                  = string
-          nsg-name                = string
-          nsg-rule-number         = string
           pip-id                  = string
           public-key              = string
           randomizer              = string
           rg-name                 = string
+          server-count            = number
+          server-name             = string
           storage-account-uri     = string
           storage-image-reference = string
           subnet-id               = string
