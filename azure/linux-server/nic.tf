13100f1b4321d4b87ae3bbe408853023886ac28f
David Sanders <dsanderscanada@gmail.com>
2020-01-26 20:38:14 -0500

    Fix nic variables for network

--- azure/linux-server/nic.tf
+++ azure/linux-server/nic.tf
@@ -1,5 +1,5 @@
 resource "azurerm_network_interface" "nic" {
-  count    = var.linux-server.nic-count
+  count    = var.linux-server.server-count
   location = var.linux-server.location
   name     = upper(
     format(
@@ -14,10 +14,10 @@ resource "azurerm_network_interface" "nic" {
 
   ip_configuration {
     name = "IPv4Config"
-    private_ip_address            = var.linux-server.private-ip-address
-    private_ip_address_allocation = var.linux-server.private-ip-alloc
-    public_ip_address_id          = var.linux-server.public-ip-id
-    subnet_id                     = var.linux-server.subnet-id
+    private_ip_address            = var.linux-server.network.private-ip-address
+    private_ip_address_allocation = var.linux-server.network.private-ip-alloc
+    public_ip_address_id          = var.linux-server.network.public-ip-id
+    subnet_id                     = var.linux-server.network.subnet-id
   }
 
   tags       = var.tags
