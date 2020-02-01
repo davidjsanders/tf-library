367a63b8e23c9d36a70f5b5d8c575729a2ee423a
David Sanders <dsanderscanada@gmail.com>
2020-02-01 16:15:11 -0500

    Fix error in jumpbox

--- azure/linux-jumpbox/main.tf
+++ azure/linux-jumpbox/main.tf
@@ -1,20 +1,19 @@
 module "jumpbox" {
     source = "../linux-server"
     linux-server = {
-        boot-diagnostics = boot-diagnostics
-
-        location   = var.linux-jumpbox.location
-        network    = {
+        boot-diagnostics = var.linux-jumpbox.boot-diagnostics
+        location         = var.linux-jumpbox.location
+        network          = {
             private-ip-address = var.linux-jumpbox.network.private-ip-address
             private-ip-alloc   = var.linux-jumpbox.network.private-ip-alloc
             public-ip-id       = azurerm_public_ip.jumpbox-pip.id
             subnet-id          = var.linux-jumpbox.network.subnet-id
         }
-        os         = var.linux-jumpbox.os
-        os-disk    = var.linux-jumpbox.os-disk
-        randomizer = var.linux-jumpbox.randomizer
-        rg-name    = var.linux-jumpbox.rg-name
-        server     = var.linux-jumpbox.server
+        os               = var.linux-jumpbox.os
+        os-disk          = var.linux-jumpbox.os-disk
+        randomizer       = var.linux-jumpbox.randomizer
+        rg-name          = var.linux-jumpbox.rg-name
+        server           = var.linux-jumpbox.server
     }
     tags         = var.tags
 }
