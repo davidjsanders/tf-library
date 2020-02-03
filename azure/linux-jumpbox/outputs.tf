47ce72ffcb91fc6f14829b335e272b129c593a51
David Sanders <dsanderscanada@gmail.com>
2020-02-02 20:42:16 -0500

    Correct module name in outputs

--- azure/linux-jumpbox/outputs.tf
+++ azure/linux-jumpbox/outputs.tf
@@ -1,13 +1,13 @@
 output "nic-id" {
-  value = module.linux-server.*.nic-ids[0]
+  value = module.jumpbox.*.nic-ids[0]
 }
 
 output "nic-name" {
-  value = module.linux-server.*.nic-names[0]
+  value = module.jumpbox.*.nic-names[0]
 }
 
 output "nic-private-ip" {
-  value = module.linux-server.*.nic-private-ips[0]
+  value = module.jumpbox.*.nic-private-ips[0]
 }
 
 output "public-ip" {
@@ -15,18 +15,18 @@ output "public-ip" {
 }
 
 output "rg-name" {
-  value = module.linux-server.rg-name
+  value = module.jumpbox.rg-name
 }
 
 output "vm-id" {
-  value = module.linux-server.*.vm-ids[0]
+  value = module.jumpbox.*.vm-ids[0]
 }
 
 output "vm-name" {
-  value = module.linux-server.*.vm-names[0]
+  value = module.jumpbox.*.vm-names[0]
 }
 
 output "vm-password" {
-  value = module.linux-server.vm-password
+  value = module.jumpbox.vm-password
 }
 
