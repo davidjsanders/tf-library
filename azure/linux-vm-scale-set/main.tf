fc2fbbcfe3d30091f022e975106c1b75733e79b4
David Sanders <dsanderscanada@gmail.com>
2020-02-03 20:10:03 -0500

    Alpha Linux VM Scale Set

---
+++ azure/linux-vm-scale-set/main.tf
@@ -0,0 +1,80 @@
+resource "azurerm_virtual_machine_scale_set" "linux-vm-scale-set" {
+  name                = upper(
+    format(
+      "VMSS-%s-%02d%s",
+      upper(var.scale-set.scale-set-name),
+      var.scale-set.randomizer
+    )
+  )
+  location            = var.scale-set.location
+  resource_group_name = var.scale-set.rg-name
+  upgrade_policy_mode = var.scale-set.upgrade-policy-mode
+
+  sku {
+    name     = var.scale-set.server.machine-size
+    tier     = var.scale-set.server.tier
+    capacity = var.scale-set.server.capacity
+  }
+
+  network_profile {
+    name    = upper(
+      format(
+          "PROFILE-%s-%02d%s",
+          upper(var.scale-set.scale-set-name),
+          var.scale-set.randomizer
+      )
+    )
+    primary = true
+
+    ip_configuration {
+      name                                   = "IPv4"
+      subnet_id                              = var.scale-set.network.subnet-id
+      load_balancer_backend_address_pool_ids = var.scale-set.network.lb-backend-address-pool-ids
+      load_balancer_inbound_nat_rules_ids    = var.scale-set.network.lb-inbound-nat-rules-ids
+    }
+  }
+
+  boot_diagnostics = {
+    storage_uri = var.scale-set.boot-diagnostics.uri
+    enabled     = var.scale-set.boot-diagnostics.enable
+  }
+
+  storage_profile_image_reference {
+    id = var.scale-set.os.storage-image-reference
+  }
+
+  storage_profile_os_disk {
+    caching           = var.scale-set.os-disk.caching
+    create_option     = var.scale-set.os-disk.create-option
+    managed_disk_type = var.scale-set.os-disk.managed-type
+  }
+
+  dynamic "storage_profile_data_disk" {
+    for_each = var.scale-set.data-disk
+    iterator = "data-disk"
+    content {
+      create_option     = data-disk.create-option
+      caching           = data-disk.caching
+      disk_size_gb      = data-disk.disk-size-gb
+      lun               = data-disk.lun
+      managed_disk_type = data-disk.managed-disk-type
+    }
+  }
+
+  os_profile {
+    computer_name_prefix = lower(format(
+        "%s%s",
+        var.scale-set.os.hostname,
+        var.scale-set.randomizer
+      ))
+    admin_username = var.scale-set.os.admin-user
+    admin_password = random_password.password.result
+    custom_data    = var.scale-set.os.custom-data
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = var.scale-set.os.disable-password-auth
+  }
+
+  tags = var.tags
+}
---
+++ azure/linux-vm-scale-set/variables.tf
@@ -0,0 +1,58 @@
+variable "scale-set" {
+  type = object(
+      {
+        boot-diagnostics = object({
+          enable = bool
+          uri    = string
+        })
+        data-disk        = list(
+          object({
+            caching        = string
+            create-option  = string
+            disk-size-gb   = number
+            lun            = number
+            managed-type   = string
+          })
+        )
+        location         = string
+        network          = object({
+          lb-backend-address-pool-ids = list(string)
+          lb-inbound-nat-rules-ids    = list(string)
+          private-ip-address          = string
+          private-ip-alloc            = string
+          public-ip-id                = string
+          subnet-id                   = string
+        })
+        os               = object({
+          admin-user              = string
+          custom-data             = string
+          disable-password-auth   = bool
+          hostname                = string
+          private-key-filename    = string
+          public-key              = string
+          storage-image-reference = string
+        })
+        os-disk          = object({
+          caching        = string
+          create-option  = string
+          disk-size-gb   = number
+          managed-type   = string
+          delete-on-done = bool
+        })
+        randomizer       = string
+        rg-name          = string
+        scale-set-name   = string
+        server           = object({
+          capacity            = number
+          machine-size        = string
+          server-count        = number
+          server-name         = string
+          tier                = string
+          upgrade-policy-mode = string
+        })
+      }
+  )
+}
+variable "tags" {
+  type = object({})
+}
