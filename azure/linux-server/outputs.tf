0dffdd04539b3ab2a70a446915ebfba2af766021
David Sanders <dsanderscanada@gmail.com>
2020-02-09 16:03:05 -0500

    Calculate hostnames for outputs

--- azure/linux-server/outputs.tf
+++ azure/linux-server/outputs.tf
@@ -1,5 +1,13 @@
 output "hostnames" {
-  value = azurerm_virtual_machine.vm.*.os_profile.computer_name
+  value = [
+    for i in range(0, var.linux-server.server.server-count) :
+      lower(format(
+            "%s-%02d%s",
+            var.linux-server.os.hostname,
+            i + 1,
+            var.linux-server.randomizer
+          ))
+  ]
 }
 
 output "nic-ids" {
