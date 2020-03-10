5f19655d669f4627df49f06690dc8818b89d1ccd
David Sanders <dsanderscanada@gmail.com>
2020-03-10 19:16:49 -0400

    Output private IP

--- gcp/linux-jumpbox/outputs.tf
+++ gcp/linux-jumpbox/outputs.tf
@@ -6,6 +6,10 @@ output "ip" {
     value = module.linux-jumpbox.ip
 }
 
+output "private-ip" {
+    value = module.linux-jumpbox.private-ip
+}
+
 output "self-link" {
     value = module.linux-jumpbox.self-link
 }
--- gcp/linux-server/outputs.tf
+++ gcp/linux-server/outputs.tf
@@ -6,6 +6,10 @@ output "ip" {
     value = var.server.public-ip ? google_compute_instance.vm.network_interface.0.access_config.0.nat_ip : "No Public IP"
 }
 
+output "private-ip" {
+    value = google_compute_instance.vm.network_interface.0.network_ip
+}
+
 output "self-link" {
     value = google_compute_instance.vm.self_link
 }
