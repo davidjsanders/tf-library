254e85e816b70b16b9149db67b9bcb334724ebed
David Sanders <dsanderscanada@gmail.com>
2020-03-10 19:40:11 -0400

    Update outputs to equal types

--- gcp/linux-server/outputs.tf
+++ gcp/linux-server/outputs.tf
@@ -1,9 +1,9 @@
 output "hostname" {
-    value = var.server.public-ip ? google_compute_instance.vm.*.hostname : "No Public IP"
+    value = var.server.public-ip ? google_compute_instance.vm.*.hostname : ["No Public IP"]
 }
 
 output "ip" {
-    value = var.server.public-ip ? google_compute_instance.vm.*.network_interface.0.access_config.0.nat_ip : "No Public IP"
+    value = var.server.public-ip ? google_compute_instance.vm.*.network_interface.0.access_config.0.nat_ip : ["No Public IP"]
 }
 
 output "private-ip" {
