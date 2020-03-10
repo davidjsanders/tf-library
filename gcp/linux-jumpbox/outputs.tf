2c092ef3209f58f19352a3153bf11ae523d5bc92
David Sanders <dsanderscanada@gmail.com>
2020-03-10 19:38:10 -0400

    Change outputs to specific instances

--- gcp/linux-jumpbox/outputs.tf
+++ gcp/linux-jumpbox/outputs.tf
@@ -1,15 +1,15 @@
 output "hostname" {
-    value = module.linux-jumpbox.hostname
+    value = module.linux-jumpbox[0].hostname
 }
 
 output "ip" {
-    value = module.linux-jumpbox.ip
+    value = module.linux-jumpbox[0].ip
 }
 
 output "private-ip" {
-    value = module.linux-jumpbox.private-ip
+    value = module.linux-jumpbox[0].private-ip
 }
 
 output "self-link" {
-    value = module.linux-jumpbox.self-link
+    value = module.linux-jumpbox[0].self-link
 }
