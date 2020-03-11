1f79c3efa7b48c4d07d0a97ecefa546524ae25cf
David Sanders <dsanderscanada@gmail.com>
2020-03-10 21:17:21 -0400

    Fix GCP Linux Jumpbox outputs

--- gcp/linux-jumpbox/outputs.tf
+++ gcp/linux-jumpbox/outputs.tf
@@ -1,15 +1,15 @@
 output "hostname" {
-    value = module.linux-jumpbox[0].hostname
+    value = module.linux-jumpbox.*.hostname[0]
 }
 
 output "ip" {
-    value = module.linux-jumpbox[0].ip
+    value = module.linux-jumpbox.*.ip[0]
 }
 
 output "private-ip" {
-    value = module.linux-jumpbox[0].private-ip
+    value = module.linux-jumpbox.*.private-ip[0]
 }
 
 output "self-link" {
-    value = module.linux-jumpbox[0].self-link
+    value = module.linux-jumpbox.*.self-link[0]
 }
