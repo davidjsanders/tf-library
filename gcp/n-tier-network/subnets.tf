91e73c6c73772f494bd92926c8987a9fa8f7d2e5
David Sanders <dsanderscanada@gmail.com>
2020-03-11 20:39:06 -0400

    Fix bug in subnet region name

--- gcp/n-tier-network/subnets.tf
+++ gcp/n-tier-network/subnets.tf
@@ -3,6 +3,6 @@ resource "google_compute_subnetwork" "subnets" {
 
   name          = var.network-info.subnets[count.index].subnet-name
   ip_cidr_range = var.network-info.subnets[count.index].cidr-block
-  region        = var.network-info.subnets[count.index].subnet-name
+  region        = var.network-info.subnets[count.index].region
   network       = google_compute_network.vnet.self_link
 }
